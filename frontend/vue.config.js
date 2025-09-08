fa40a4db3140549c8311fd8805f189effd70a68f
Hernandez, Lans Lorence N. <hernandezlanslorence@gmail.com>
2025-09-09 04:28:55 +0800

    Refactor code structure for improved readability and maintainability

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,61 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'JM GARIS STORE'
+        return args
+      })
+  },
+  pwa: {
+    name: 'JM GARIS STORE',
+    short_name: 'JM GARIS',
+    themeColor: '#2563eb',
+    msTileColor: '#2563eb',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'default',
+    manifestOptions: {
+      name: 'JM GARIS STORE',
+      short_name: 'JM GARIS',
+      display: 'standalone',
+      theme_color: '#2563eb',
+      background_color: '#ffffff',
+      start_url: '/',
+      icons: [
+        {
+          src: '/img/icons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: '/img/icons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: '/img/icons/android-chrome-maskable-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'maskable'
+        },
+        {
+          src: '/img/icons/android-chrome-maskable-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
+        }
+      ]
+    },
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/mstile-150x150.png'
+    }
+  }
 })
